efc11322c02d68d98fbb903c5ac5f4be2a6ea6b1
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-11-03 09:17:22 +0100

    Update to OMNI ROM

--- omni_p4.mk
+++ omni_p4.mk
@@ -2,15 +2,15 @@
 $(call inherit-product, device/samsung/p4/p4.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_p4
+PRODUCT_NAME := omni_p4
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := p4
 PRODUCT_MODEL := GT-P7500
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,4 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_p4.mk
-
+add_lunch_combo omni_p4-userdebug
