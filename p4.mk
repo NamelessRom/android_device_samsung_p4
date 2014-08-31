13320b391f8c5e9dc589ef1a0941fb9f6b4b41a6
jumoog <kilianvonpflugk@gmail.com>
2014-08-31 22:09:49 +0200

    use own blobs
    Change-Id: I6390945fb3992d6937d3029c818e87545a5afb3a

--- p4.mk
+++ p4.mk
@@ -42,7 +42,7 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product, device/samsung/p4-common/p4-common.mk)
 
-$(call inherit-product, vendor/samsung/p4/p4-vendor.mk)
+$(call inherit-product, vendor/samsung_own/p4/p4-vendor.mk)
 
 # Cameradata
 PRODUCT_COPY_FILES += \
